93903b4ccd2e52de6ea6708fb5cdd7ab0b9ec754
Chris Wong <cwong@pivotal.io>
2018-11-13 17:10:03 -0500

    Make compliance compliant [#161655190]

--- redirects.rb
+++ redirects.rb
@@ -3,12 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
-
-r301 %r{/addon-ipsec/1-8-[\d]+/(.*)}, "/addon-ipsec/1-8/$1"
-
-r301 %r{/addon-ipsec/1-7-[\d]+/(.*)}, "/addon-ipsec/1-7/$1"
-
-r301 %r{/addon-ipsec/1-6/(.*)}, 'http://docs.pivotal.io/archives/addon-ipsec-1.6.pdf'
-
-r301 %r{/addon-ipsec/1-5/(.*)}, 'http://docs.pivotal.io/archives/addon-ipsec-1.5.pdf'
+r301 %r{/addon-compliance-tools/(?![\d-]+)(.*)}, "/addon-compliance-tools/1-0/$1"
