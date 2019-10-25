80301ba20ab4edc9260313def345c028aafc2705
Chris Wong <cwong@pivotal.io>
2019-10-25 15:20:23 -0400

    Add 1.2 to master book [#169303761]

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/addon-compliance-tools/(?![\d-]+)(.*)}, "/addon-compliance-tools/1-0/$1"
+r301 %r{/addon-compliance-tools/(?![\d-]+)(.*)}, "/addon-compliance-tools/1-2/$1"
