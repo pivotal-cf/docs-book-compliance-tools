87c202d85df209de2774376274491f982406609f
Miranda Limonczenko <mlimonczenko@pivotal.io>
2020-05-07 10:25:35 -0700

    remove redirects [#172510373]

--- redirects.rb
+++ redirects.rb
@@ -2,5 +2,3 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-
-r301 %r{/addon-compliance-tools/(?![\d-]+)(.*)}, "/addon-compliance-tools/1-2/$1"
