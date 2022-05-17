19ea62b8c6fb3702a3ed472f8b9580b55dca65a7
snneji <snneji@vmware.com>
2022-05-17 16:43:41 +0100

    Fix redirect for compliance scanner [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -1,2 +1,2 @@
 # Redirect all traffic to VMware site
-r301 %r{(.*)}, 'https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-index.html'
+r301 %r{/addon-compliance-tools/1-2/(.*)}, 'https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-$1'
