442419ade0ebd0bfb41e640f392ab72f58a996c2
Samia Nneji <snneji@vmware.com>
2022-05-26 10:05:10 +0100

    Compliance tools adding more redirects [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -1,2 +1,4 @@
 # Redirect all traffic to VMware site
-r301 %r{/addon-compliance-tools/1-2/(.*)}, 'https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-$1'
+r301 %r{/addon-compliance-tools/1-2/(.*)}, "https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-$1"
+r301 %r{/addon-compliance-tools/1-2/}, "https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-index.html"
+r301 %r{/addon-compliance-tools/1-2}, "https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.2/addon-compliance-tools/GUID-index.html"
