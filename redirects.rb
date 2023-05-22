1baad188472638802ab08a257867ccc0f764de60
Samia Nneji <snneji@vmware.com>
2023-05-22 15:19:34 +0100

    [#185146911] Fix redirect

--- redirects.rb
+++ redirects.rb
@@ -5,4 +5,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirect from docs.pivotal.io/addon-compliance-tools/1-0 to docs.vmware.com Compliance Tools PDF
 
-r301  %r{/addon-compliance-tools/1-0)}, "https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.0/compliance-tools-1-0.pdf"
+r301  %r{/addon-compliance-tools/1-0}, "https://docs.vmware.com/en/Compliance-Scanner-for-VMware-Tanzu/1.0/compliance-tools-1-0.pdf"
